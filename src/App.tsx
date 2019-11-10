bc9412b29c95a8fd17693b1218e25693344f7855
Neilyo <kk_cyndi@126.com>
2019-11-10 23:35:53 +0800

    1st Example: Basic Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,58 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+const Home: React.FC = () => <h2>Home</h2>;
+
+const About: React.FC = () => <h2>About</h2>;
+
+const Users: React.FC = () => <h2>Users</h2>;
+
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+
+
+/* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,8 +67,5 @@ const App: React.FC = () => {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
-
+    </div> */
 export default App;
